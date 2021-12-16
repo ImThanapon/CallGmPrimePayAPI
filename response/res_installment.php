7238088ef47df9b734b438d0889ff6edaaf70931
Thanapon Visedsang <Thanapon.vi@ku.th>
2021-12-17 00:59:29 +0700

    เพิ่มอัพเดทการชำระเงินแล้ว ของ installment และ QR code

--- response/res_installment.php
+++ response/res_installment.php
@@ -1,9 +1,19 @@
 <?php
-    $respFile = fopen("log/bg-log_qr.txt", "w") or die("Unable to open file!");
+    require_once('../connetion.php');
+
+    $respFile = fopen("log/res-log_installment.txt", "w") or die("Unable to open file!");
 
     $json_str = file_get_contents('php://input');
     fwrite($respFile, $json_str . "\n\n");
     $json_obj = json_decode($json_str);
+
+    $resultCode = $json_obj->resultCode;
+    $amount = $json_obj->amount;
+    $referenceNo = $json_obj->referenceNo;
+    $gbpReferenceNo = $json_obj->gbpReferenceNo;
+    $currencyCode = $json_obj->currencyCode;
+    $totalAmount = $json_obj->totalAmount;
+    $thbAmount = $json_obj->thbAmount;
 
     fwrite($respFile, "resultCode=" . $json_obj->resultCode . "\n");
     fwrite($respFile, "amount=" . $json_obj->amount . "\n");
@@ -16,7 +26,27 @@
     fclose($respFile);
 
     
-        echo $json_obj->resultCode;
+    echo $resultCode;
+
+    $date_pay = date("y-m-d H:i:s");
+    if($resultCode == '00'){
+        $sql_update = " UPDATE order_detail 
+                        SET date_payment = '$date_pay',
+                            result_code = '$resultCode',
+                            amount = '$amount',
+                            gbp_ref_no = ' $gbpReferenceNo',
+                            currency_code = '$currencyCode' 
+                        WHERE ref_no = '$referenceNo'";
+
+        $query = mysqli_query($con,$sql_update);
+        if($query) {
+
+            // echo "Record add successfully";
+        }else{
+        // echo 'เพิ่มข้อมูลไม่สำเร็จ';
+        }
+        mysqli_close($con);
+    }
 
     
 
--- response/res_qr.php
+++ response/res_qr.php
@@ -1,19 +1,50 @@
 <?php
+    
+    require_once('../connetion.php');
+    $respFile = fopen("log/resp-qr-log.txt", "w") or die("Unable to open file!");
 
-$respFile = fopen("log/resp-qr-log.txt", "w") or die("Unable to open file!");
+    $json_str = file_get_contents('php://input');
+    fwrite($respFile, $json_str . "\n\n");
 
-$json_str = file_get_contents('php://input');
-fwrite($respFile, $json_str . "\n\n");
+    $json_obj = json_decode($json_str);
 
-$json_obj = json_decode($json_str);
+    $resultCode = $json_obj->resultCode;
+    $amount = $json_obj->amount;
+    $referenceNo = $json_obj->referenceNo;
+    $gbpReferenceNo = $json_obj->gbpReferenceNo;
+    $currencyCode = $json_obj->currencyCode;
+    
+    fwrite($respFile, "resultCode=" . $json_obj->resultCode . "\n");
+    fwrite($respFile, "amount=" . $json_obj->amount . "\n");
+    fwrite($respFile, "referenceNo=" . $json_obj->referenceNo . "\n");
+    fwrite($respFile, "gbpReferenceNo=" . $json_obj->gbpReferenceNo . "\n");
+    fwrite($respFile, "currencyCode=" . $json_obj->currencyCode . "\n");
 
-fwrite($respFile, "resultCode=" . $json_obj->resultCode . "\n");
-fwrite($respFile, "amount=" . $json_obj->amount . "\n");
-fwrite($respFile, "referenceNo=" . $json_obj->referenceNo . "\n");
-fwrite($respFile, "gbpReferenceNo=" . $json_obj->gbpReferenceNo . "\n");
-fwrite($respFile, "currencyCode=" . $json_obj->currencyCode . "\n");
+    fclose($respFile);
 
-fclose($respFile);
+    echo $json_obj->resultCode;
 
-echo $json_obj->resultCode;
+
+
+    $date_pay = date("y-m-d H:i:s");
+    if($resultCode == '00'){
+        $sql_update = " UPDATE order_detail 
+                        SET date_payment = '$date_pay',
+                            result_code = '$resultCode',
+                            amount = '$amount',
+                            gbp_ref_no = ' $gbpReferenceNo',
+                            currency_code = '$currencyCode' 
+                        WHERE ref_no = '$referenceNo'";
+
+        $query = mysqli_query($con,$sql_update);
+        if($query) {
+
+            // echo "Record add successfully";
+        }else{
+        // echo 'เพิ่มข้อมูลไม่สำเร็จ';
+        }
+        mysqli_close($con);
+    }
+
+
 ?>
